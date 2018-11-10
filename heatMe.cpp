bf3ff6f7a71aafe596303d55d75ffad567cfea37
Jacob Dorais <jacobdorais25@hotmail.com>
2018-11-10 10:55:13 -0500

    now use the cpu at around 98 percent with choosen number of threads

--- heatMe.cpp
+++ heatMe.cpp
@@ -2,38 +2,59 @@
 #include <time.h>
 #include <windows.h>
 #include <string.h>
+#include <sstream>
 
 using namespace std;
 
-void func(string wow) {
+void func(int* sec) {
   cout << "Hello ";
-  wow = "!";
   clock_t timer;
   timer = clock();
-  while(clock() < 100);
+  int i = 0;
+  while(clock() < (*sec)*1000){
+    i++;
+    i--;
+  }
 }
 
-void join(HANDLE t) {
-  WaitForSingleObject(t, INFINITE);
-  return;
-}
+// params
+// first arg must be number of nThreads
+// second arg must be amount of desired seconds
 
+//
 int main(int argc, _TCHAR* argv[]) {
-    clock_t timer;
-    string wow;
+int nThreads = 1; // default value
+int sec = 1;//default value
 
-    HANDLE t = CreateThread(
-    NULL,                                            //LPSECURITY_ATTRIBUTES   lpThreadAttributes,
-    0,                                               //SIZE_T                  dwStackSize,
-    (LPTHREAD_START_ROUTINE)func,                    //LPTHREAD_START_ROUTINE  lpStartAddress,
-    &wow,                                         //__drv_aliasesMem LPVOID lpParameter,
-    0,                                               //DWORD                   dwCreationFlags,
-    NULL                                             //LPDWORD                 lpThreadId
-  );
+if(argv[1]){
+  stringstream s(argv[1]);
+  s >> nThreads;
+}
+if(argv[2]){
+  stringstream s(argv[2]);
+  s >> sec;
+}
 
-  join(t);
-  cout << "World";
-  cout << wow << endl;
+  clock_t timer;
+  HANDLE* t = new HANDLE[nThreads];
+  for(int i = 0; i < nThreads; i++){
+
+    t[i] = CreateThread(
+      NULL,                                            //LPSECURITY_ATTRIBUTES   lpThreadAttributes,
+      0,                                               //SIZE_T                  dwStackSize,
+      (LPTHREAD_START_ROUTINE)func,                    //LPTHREAD_START_ROUTINE  lpStartAddress,
+      &sec,                                         //__drv_aliasesMem LPVOID lpParameter,
+      0,                                               //DWORD                   dwCreationFlags,
+      NULL                                             //LPDWORD                 lpThreadId
+    );
+  }
+
+
+  for(int i = 0; i < nThreads; i++) {
+   WaitForSingleObject(t[i], INFINITE);
+}
+delete[] t;
+  cout << "World!";
 
   CloseHandle(t);
 }
