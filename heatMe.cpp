27508215e4cf654d8a84924d65e7fceffa1c9b97
Jacob Dorais <jacobdorais25@hotmail.com>
2019-10-29 09:47:47 -0400

    adds option parser

--- heatMe.cpp
+++ heatMe.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+void parseOptions(int argc, _TCHAR* argv[]);
+
 void func(int* sec) {
   cout << "Hello\n";
   clock_t timer = clock();
@@ -20,31 +22,19 @@ void func(int* sec) {
 // first arg must be amount of desired seconds
 // second arg must be number of nThreads
 
-//
-int main(int argc, _TCHAR* argv[]) {
-unsigned int nThreads = 1; // default value
-unsigned int sec = 0;//default value
+// globals due to options
 
+unsigned int nThreads = 1; // default value
+unsigned int sec = 1;//default value
+
+int main(int argc, _TCHAR* argv[]) {
 #ifdef WIN32
     SYSTEM_INFO sysinfo;
     GetSystemInfo(&sysinfo);
     nThreads = sysinfo.dwNumberOfProcessors;
 #endif
 
-if(argc > 1){
-  stringstream s(argv[1]);
-  s >> sec;
-}
-else
-{
-  cout << "How many seconds to keep you warm ?" << endl;
-  cin >> sec;
-}
-
-if(argc > 2){
-  stringstream s(argv[2]);
-  s >> nThreads;
-}
+parseOptions(argc, argv);
 
 cout << nThreads << " threads will heat you up during " << sec << " seconds" << endl;
 
@@ -70,4 +60,28 @@ delete[] t;
   cout << "World!";
 
   CloseHandle(t);
+}
+
+void parseOptions(int argc, _TCHAR* argv[])
+{
+  for(int i = 1; i < argc; i++)
+  {
+    string arg;
+    stringstream argStream(argv[i]);
+    argStream >> arg;
+    if (arg == "-s")
+    {
+      stringstream s(argv[++i]);
+      s >> sec;
+    }
+    else if (arg == "-n")
+    {
+      stringstream s(argv[++i]);
+      s >> nThreads;
+    }
+    else
+    {
+      cerr << "Usage: " << argv[0] << " [-n NUMBEROFTHREADS] [-s NUMBEROFSECONDS]\n";
+    }
+  }
 }
